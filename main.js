f144e699327997fd88b3ab2baa6ccf9dde2efdf9
Rodrigo Cintra domingos da Silva <94804801+Rodrigo202134@users.noreply.github.com>
2022-11-11 01:34:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ document.querySelector("#card").innerHTML =
 createCard("20/11","domingo",createGame('qatar','13:00','ecuador'))+
 createCard("21/11","segunda",createGame('iran','10:00','england')+createGame('senegal','13:00','netherlands')+createGame('united states','16:00','wales'))+
 createCard("22/11","terça",createGame('argentina','07:00','saudi')+createGame('denmark','10:00','tunisia')+createGame('mexico','13:00','poland')+createGame('france','16:00','australia'))+
-createCard("23/11","quinta",createGame('morocco','07:00','croatia')+createGame('germany','10:00','japan')+createGame('spain','13:00','costa rica')+createGame('belgium','16:00','canada'))+
+createCard("23/11","quarta",createGame('morocco','07:00','croatia')+createGame('germany','10:00','japan')+createGame('spain','13:00','costa rica')+createGame('belgium','16:00','canada'))+
 createCard("24/11","quinta",createGame('switzerland','07:00','cameroon')+createGame('uruguay','10:00','south korea')+createGame('portugal','13:00','ghana')+createGame('brazil','16:00','serbia'))+
 createCard("25/11","sexta",createGame('wales','07:00','iran')+createGame('qatar','10:00','senegal')+createGame('netherlands','13:00','ecuador')+createGame('england','16:00','united states'))+
 createCard("26/11","sabado",createGame('tunisia','07:00','australia')+createGame('poland','10:00','argentina')+createGame('france','13:00','denmark')+createGame('saudi','16:00','mexico'))+
